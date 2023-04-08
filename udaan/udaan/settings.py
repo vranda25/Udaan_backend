dd1cc9d62cb565b5864892a86637b1bf1c0095cc
Vranda Mahajan <202052349@iiitvadodara.ac.in>
2023-04-09 01:33:28 +0530

    Update settings.py

--- udaan/udaan/settings.py
+++ udaan/udaan/settings.py
@@ -159,7 +159,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'snohara223@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'eywoyukouwxcsjaj'
 
 
 REST_FRAMEWORK = {
