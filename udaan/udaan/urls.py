fb45755dc034fddcbdede57728a2906ae8969a4f
Vranda Mahajan <202052349@iiitvadodara.ac.in>
2023-04-09 10:53:47 +0530

    urls

--- udaan/udaan/urls.py
+++ udaan/udaan/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
@@ -23,3 +24,4 @@ urlpatterns = [
     path('api/service/', include('service.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += staticfiles_urlpatterns()
